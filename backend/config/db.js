46eff4c7982cdfe7ff1c537abefb7102bf64d2af
Emuchay <127212673+Munchisis@users.noreply.github.com>
2026-04-29 10:37:53 +0100

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://Bistro_db:7zW0v5rcPxyMbXnH@cluster0.lkkzndu.mongodb.net/MUNCH-BISTRO').then(()=> console.log("DB Connected"));
-}
