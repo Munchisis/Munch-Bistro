18e2bfd9fb1b00760ffc3b8d7a7ced9c9e98d9a9
Chigozie <kelmuchy2@gmail.com>
2026-06-12 09:52:43 +0100

    fix: Export StoreContextProvider for proper usage in other components

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import StoreContext from "./storeContext";
 
-const StoreContextProvider = (props) => {
+ export const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const url = "https://munch-bistro-backend.onrender.com";
   const [token, setToken] = useState("");
